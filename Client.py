8f73328e52d23ec90bc37e08d4415c09c9607fa4
mayacohen7 <maya.cohen9@mail.huji.ac.il>
2024-08-24 18:22:25 +0300

    up

--- Client.py
+++ Client.py
@@ -6,7 +6,7 @@ from Utils import Utils
 
 class Client:
     def __init__(self, main_server):
-        self.main_server = main_server  # Reference to the main server
+        self.main_server = main_server
 
     def add_file(self, file):
         """
