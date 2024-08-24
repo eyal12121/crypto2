fafabbd8201562ad396a0781d20498039a86f90a
mayacohen7 <maya.cohen9@mail.huji.ac.il>
2024-08-24 18:01:53 +0300

    reformat

--- Utils.py
+++ Utils.py
@@ -5,17 +5,14 @@ class Utils:
 
     @staticmethod
     def hash_data(data):
+        """
+        Hashes the given data.
+        """
         return hashlib.sha256(data).hexdigest()
 
     @staticmethod
     def hash_concat(left, right):
+        """
+        Concatenates the given data and then hashes it.
+        """
         return hashlib.sha256((left + right).encode('utf-8')).hexdigest()
-
-    @staticmethod
-    def verify_data(data, proof, root_hash):
-        hashed = Utils.hash_data(data)  # Hash of the data chunk
-        for sibling in proof:
-            hashed = Utils.hash_concat(hashed, sibling)  # Combine with sibling hashes
-        return hashed == root_hash  # Verify against the root hash
-
-    # Function to build a Merkle tree and get root hash
