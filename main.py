f04c368385b7fb0cc9210e5bb2b27b5d23a9de94
mayacohen7 <maya.cohen9@mail.huji.ac.il>
2024-08-30 13:46:19 +0300

    BugFix

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 from Client import Client
 from MainServer import MainServer
 
+NUM_CLIENTS = 3
+NUM_CHECKS = 5
+
 
 def main():
     main_server = MainServer()
-
-    client = Client(main_server)
-    client2 = Client(main_server)
+    clients = []
+    output_files = []
+    for i in range(NUM_CLIENTS):
+        clients.append(Client(main_server))
     file_path = "example.txt"
     file_path2 = "example2.txt"
-    client.add_file(file_path)
-    client2.add_file(file_path2)
-    output_path = "checking.txt"
-    output_path2 = "checking2.txt"
-    output_path3 = "checking3.txt"
-    output_path4 = "checking4.txt"
-    client.request_file(file_path, output_path)
-    client2.request_file(file_path, output_path2)
-    client.request_file(file_path2, output_path3)
-    client2.request_file(file_path2, output_path4)
-    assert (client.remove_file(file_path))
-    assert (not client.remove_file(file_path2))
-    # server changed info
-    # erasure encoding
+    for i in range(NUM_CHECKS):
+        out_name = "checkings" + str(i) + ".txt"
+        output_files.append(out_name)
+
+    clients[0].add_file(file_path)  # addition of file into system
+    clients[1].add_file(file_path2)  # addition of file into system
+    clients[0].request_file(file_path, output_files[0])  # client requests file it added to system
+    clients[1].request_file(file_path, output_files[1])  # client requests file it added to system
+    clients[0].request_file(file_path2, output_files[2])  # client request file it did not add to system
+    clients[1].request_file(file_path2, output_files[3])  # client request file it did not add to system
+    assert (clients[0].remove_file(file_path))  # client asks to remove file it added to system
+    assert (not clients[0].remove_file(file_path2))  # client asks to remove file it did not add to system
+    clients[2].add_file(file_path)  # addition of removed file into system by different client
+    clients[0].request_file(file_path, output_files[4])  # client request file it did not add to system
+    assert (not clients[0].remove_file(file_path))  # client asks to remove file that it did not add to system
+    # server changed info / fall
     # dynamic server amount (serverManager)?
+    # add test funcs for different case scenarios
+
 
 if __name__ == "__main__":
     main()
